0c43dc62ec4734c4942a6f5cb287c25e42c518c1
Tristan <tristandavisbrody@gmail.com>
2021-07-02 23:03:43 -0700

    -

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import fruits from './foods.js';
+import { choice, remove } from './helpers.js';
+
+const randomFruit = choice(fruits);
+console.log(`I'd like one ${randomFruit}, please`);
+console.log(`Here you go: ${randomFruit}`);
+console.log(`Delicious. May I have another?`);
+remove(fruits, randomFruit);
+console.log(`I'm sorry, we're all out. We have ${fruits.length} left`);
